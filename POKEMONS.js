461291cbda5026ee995e02d0f224ef42dd43087e
Borja Gonzalez <borja@bgon.es>
2022-04-27 21:18:54 +0200

    add pokemons file

---
+++ POKEMONS.js
@@ -0,0 +1,20 @@
+const POKEMONS = [
+  {
+    id: 1,
+    name: "bulbasaur",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/1.png"
+  },
+  {
+    id: 4,
+    name: "charmander",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/4.png"
+  },
+  {
+    id: 7,
+    name: "squirtle",
+    image:
+      "https://i.kym-cdn.com/entries/icons/original/000/019/837/CWp2zVTXIAAHgWV.jpg"
+  }
+];
